49d02ec275a1b0ce812d0a3d9ff95f8b9fef769e
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2021-06-26 23:20:04 +0100

    exercise 3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector(".num-users")
 
-const getData = async function() {
-	const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function(numUsers) {
+	const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
 	const data = await usersRequest.json();
 	const userResults = data.results;
 	displayUsers(userResults);
@@ -15,12 +16,17 @@ const displayUsers = function(userResults) {
 		const country = user.location.country;
 		const name = user.name.first;
 		const imageUrl = user.picture.medium;
-		const userDiv = document.createElement("div")
+		const userDiv = document.createElement("div");
 		userDiv.innerHTML = `
         <h3>${name}</h3>
         <p>${country}</p>
         <img src=${imageUrl} alt="User avatar" />
     	`;
-    	randomFolks.append(userDiv)
+    	randomFolks.append(userDiv);
 	}
 }
+
+selectUserNumber.addEventListener("change", function(e) {
+	const numUsers = e.target.value;
+	getData(numUsers)
+})
